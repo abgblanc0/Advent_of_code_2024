b7c13c1f7bf123ded5b26e1f6bcebd50ada2237d
Alejandro <abg_blanco@hotmail.com>
2024-12-10 15:18:17 +0100

    Day 08.2

--- src/days/day08/part2.rs
+++ src/days/day08/part2.rs
@@ -1,15 +1,5 @@
 use std::{collections::{HashMap, HashSet}, fs};
 
-fn print_matrix(input: &mut Vec<Vec<char>>) {
-  for x in 0..input.len() {
-    for y in 0..input[x].len() {
-      print!("{}", input[x][y]);
-    }
-    println!();
-  }
-}
-
-
 pub fn solution() {
   let mut input: Vec<Vec<char>> = fs::read_to_string("src/days/day08/input.txt").unwrap().lines().map(|line| line.chars().map(|c| c).collect()).collect();
   let mut antennas: HashMap<char, Vec<(usize, usize)>> = HashMap::new();
@@ -32,28 +22,27 @@ pub fn solution() {
       input[coord.0][coord.1] = '#';
     }
   });
-  print_matrix(&mut input);
   println!("8.2: {}", result.len());
 }
 
 
-fn minus(input: &mut Vec<Vec<char>> ,coord: (usize, usize), diff: (usize, usize)) -> HashSet<(usize, usize)> {
+fn minus(input: &mut Vec<Vec<char>> ,coord: (usize, usize), diff: (isize, isize)) -> HashSet<(usize, usize)> {
   let mut result: HashSet<(usize,usize)> = HashSet::new();
 
   let c = *input.get(coord.0).unwrap_or(&vec!['!']).get(coord.1).unwrap_or(&'!');
   if c != '!' { result.insert(coord); } else { return result }
-  let coord: (usize, usize) = (coord.0.wrapping_sub(diff.0), coord.1.wrapping_sub(diff.1));
+  let coord: (usize, usize) = ((coord.0 as isize - diff.0) as usize, (coord.1 as isize - diff.1) as usize);
   
   result.extend(minus(input, coord, diff));
   result
 }
 
-fn plus(input: &mut Vec<Vec<char>> ,coord: (usize, usize), diff: (usize, usize)) -> HashSet<(usize, usize)> {
+fn plus(input: &mut Vec<Vec<char>> ,coord: (usize, usize), diff: (isize, isize)) -> HashSet<(usize, usize)> {
   let mut result: HashSet<(usize,usize)> = HashSet::new();
   
   let c = *input.get(coord.0).unwrap_or(&vec!['!']).get(coord.1).unwrap_or(&'!');
   if c != '!' { result.insert(coord); } else { return result }
-  let coord: (usize, usize) = (coord.0 + diff.0, coord.1 + diff.1);
+  let coord: (usize, usize) = ((coord.0 as isize + diff.0) as usize, (coord.1 as isize + diff.1) as usize);
   result.extend(plus(input, coord, diff));
   result
 }
@@ -62,7 +51,7 @@ fn antinodes(coords: &Vec<(usize, usize)>, input: &mut Vec<Vec<char>>) -> HashSe
   let mut result: HashSet<(usize, usize)> = HashSet::new();
   for x in 0..coords.len() {
     for y in x+1..coords.len() {
-      let diff = (coords[x].0.abs_diff(coords[y].0), coords[x].1.abs_diff(coords[y].1));
+      let diff = (coords[x].0 as isize - coords[y].0 as isize, coords[x].1 as isize - coords[y].1 as isize);
       
       result.extend(minus(input, coords[y], diff));
       result.extend(plus(input, coords[y], diff));
